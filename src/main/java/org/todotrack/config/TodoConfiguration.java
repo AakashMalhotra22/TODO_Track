a87835d36d61cb95306d61a3e43e257f3bdefe6c
aakashmalhotra22 <aakashmalhotra2863@gmail.com>
2023-12-31 20:08:15 +0530

    configuring mysql

--- src/main/java/org/todotrack/config/TodoConfiguration.java
+++ src/main/java/org/todotrack/config/TodoConfiguration.java
@@ -1,7 +1,19 @@
 package org.todotrack.config;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.core.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 
 public class TodoConfiguration extends Configuration
 {
+    private DataSourceFactory database = new DataSourceFactory();
 
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.database = dataSourceFactory;
+    }
 }
